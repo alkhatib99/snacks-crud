5fa3d7f7651e4174ba3043436a5405935c3d8e6a
Alkhatib99 <aboodjob@gmail.com>
2022-05-17 06:15:59 +0300

    add the templates

--- snacks_crud_project/snacks/models.py
+++ snacks_crud_project/snacks/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-from django.
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 # Create your models here.
 
 
 class Snack(models.Model):
-   pass 
+    title = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('snack_detail', args=[str(self.id)])
--- snacks_crud_project/snacks_crud_project/settings.py
+++ snacks_crud_project/snacks_crud_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,12 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+## Built -in
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+## Core 
+    'snacks',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'snacks_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILES_DIRS=[
+
+# BASE_DIR/'static'
+
+# ]
+ 
+
+
+STATIC_ROOT=BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
